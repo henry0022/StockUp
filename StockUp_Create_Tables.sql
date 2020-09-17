059f1cb3456450fdeefc9fd34cb4eb7722483819
henry0022 <henryjameschampion@gmail.com>
2020-09-17 22:25:28 +0200

    1. added passwords to to create table file. 2. Made sql file to drop tables

--- StockUp_Create_Tables.sql
+++ StockUp_Create_Tables.sql
@@ -15,6 +15,8 @@ admin_Name varchar(45) not null,
 admin_Surname varchar(45) not null,
 admin_PhoneNumber int(10) not null,
 company_ID int not null,
+username varchar(45) not null unique,
+password varchar(45) not null,
 primary key (admin_ID),
 foreign key (company_ID) references Company (company_ID)
 );
@@ -25,6 +27,8 @@ dispatch_Name varchar(45) not null,
 dispatch_Surname varchar(45) not null,
 dispatch_PhoneNumber int(10) not null,
 company_ID int not null,
+username varchar(45) not null unique,
+password varchar(45) not null,
 primary key (dispatch_ID),
 foreign key (company_ID) references Company (company_ID)
 );
@@ -52,6 +56,8 @@ staff_Name varchar(45) not null,
 staff_Surname varchar(45) not null,
 staff_PhoneNumber int(10) not null,
 store_ID int not null,
+username varchar(45) not null unique,
+password varchar(45) not null,
 primary key (staff_ID),
 foreign key (store_ID) references Store (store_ID)
 );
@@ -76,15 +82,15 @@ foreign key (order_ID) references Orders (order_ID)
 insert into Company (company_Name, company_Address, company_PhoneNumber)
 values	('Elgin Free Range Chickens', 'Industrial Rd, Grabouw 7160', '0218592795');
 
-insert into Admins (admin_Name, admin_Surname, admin_PhoneNumber, company_ID)
-values	('Jim', 'Adams', '0832965736', '001'),
-		('Bob', 'Stevens', '0735992902', '001'),
-        ('Mandy', 'Shore', '0846863495', '001');
+insert into Admins (admin_Name, admin_Surname, admin_PhoneNumber, company_ID, username, password)
+values	('Jim', 'Adams', '0832965736', '001', 'adminOne', 'P@ssword123'),
+		('Bob', 'Stevens', '0735992902', '001', 'adminTwo', 'P@ssword123'),
+        ('Mandy', 'Shore', '0846863495', '001', 'adminThree', 'P@ssword123');
 
-insert into Dispatch (dispatch_Name, dispatch_Surname, dispatch_PhoneNumber, company_ID)
-values	('Adam', 'Jones', '0842139879', '001'),
-		('Eve', 'Stones', '0734672953', '001'),
-        ('John', 'Mecury', '0716623845', '001');
+insert into Dispatch (dispatch_Name, dispatch_Surname, dispatch_PhoneNumber, company_ID, username, password)
+values	('Adam', 'Jones', '0842139879', '001', 'dispatchOne', 'P@ssword123'),
+		('Eve', 'Stones', '0734672953', '001', 'dispatchTwo', 'P@ssword123'),
+        ('John', 'Mecury', '0716623845', '001', 'dispatchThree', 'P@ssword123');
 
 insert into Product (product_Name, product_Description, company_ID)
 values	('whole chicken', 'A high quality whole free range chicken.', '001'),
@@ -97,10 +103,10 @@ values	('Woolworths', 'Suikerbos St', '0219196969'),
 		('Pick n Pay', 'Amandel and Bottelary St Soneike', '0219065700'),
         ('Checkers', '85 Kloof Ave', '0219007640');
 
-insert into Staff (staff_Name, staff_Surname, staff_PhoneNumber, store_ID)
-values	('James', 'Jameson', '0831252543', '002'),
-		('Mary', 'Scarlet', '0847285377', '001'),
-        ('Amy', 'Hendricks', '0712993475', '003');
+insert into Staff (staff_Name, staff_Surname, staff_PhoneNumber, store_ID, username, password)
+values	('James', 'Jameson', '0831252543', '002', 'staffOne', 'P@ssword123'),
+		('Mary', 'Scarlet', '0847285377', '001', 'staffTwo', 'P@ssword123'),
+        ('Amy', 'Hendricks', '0712993475', '003', 'staffThree', 'P@ssword123');
 
 insert into Orders (order_Date, store_ID)
 values	('2017-07-30', '003'),
---
+++ StockUp_Drop_Tables.sql
@@ -0,0 +1,11 @@
+
+use StockUp_db;
+
+drop table if exists Admins;
+drop table if exists Dispatch;
+drop table if exists OrderDetails;
+drop table if exists Product;
+drop table if exists Company;
+drop table if exists Staff;
+drop table if exists Orders;
+drop table if exists Store;
